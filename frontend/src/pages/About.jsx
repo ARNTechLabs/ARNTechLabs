54404956a6f11d19a6a1ae1964e24924d46d74c6
Rahulbiradar9 <123047415+Rahulbiradar9@users.noreply.github.com>
2026-02-12 10:53:50 +0530

    added about page
    1>installed react router dom
    2>added page to app
    3>cleared home page

---
+++ frontend/src/pages/About.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div>
+      <h1>Get to know us</h1>
+      <p>
+        <strong>ED-tech</strong> company.
+      </p>
+
+      <h2>Our mission</h2>
+      <p>
+        Software development is an iterative process, and the mission includes
+        continually improving software solutions.
+      </p>
+
+      <h2>Our vision</h2>
+      <p>
+        The vision of software development is to create innovative solutions
+        that push the boundaries of technology.
+      </p>
+    </div>
+  );
+};
+
+export default About;
